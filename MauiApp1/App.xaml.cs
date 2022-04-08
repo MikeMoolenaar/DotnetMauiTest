ec45d976b7cd668a94a3436d663b8e6b574c4383
Mike <Mike@DESKTOP-VU32T83>
2022-04-08 20:38:12 +0100

    Add project files.

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiApp1;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+#if WINDOWS
+		if (App.Current.UserAppTheme == AppTheme.Unspecified)
+			App.Current.UserAppTheme = AppTheme.Light;
+#endif
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public bool IsDarkMode => App.Current.UserAppTheme == AppTheme.Dark;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+		CounterLabel.Text = $"Current count: {count}";
+
+		SemanticScreenReader.Announce(CounterLabel.Text);
+	}
+
+    private void Switch_Toggled(object sender, ToggledEventArgs e)
+    {
+		App.Current.UserAppTheme = e.Value ? AppTheme.Dark : AppTheme.Light;
+	}
+}
+
