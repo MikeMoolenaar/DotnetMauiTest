a80a5f857044cc21a16ac4d14e9792397cd9d3f3
MiikeMoolenaar <mmoolenaar9@gmail.com>
2022-04-13 21:05:01 +0100

    Update for Maui RC

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -15,8 +15,8 @@ public partial class App : Application
 		// There's no other way to set the window size on Windows at the moment. 
 		// A better method is comming with https://github.com/dotnet/maui/pull/4942.
 #if WINDOWS
-		//MainPage.WidthRequest = 450;
-		//MainPage.HeightRequest = 800;
+		MainPage.WidthRequest = 450;
+		MainPage.HeightRequest = 800;
 #endif
 	}
 }
